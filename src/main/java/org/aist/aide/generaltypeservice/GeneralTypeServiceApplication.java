8bcb20eea1a836dedde8053fecbfa9b312340098
Justin Phillips <euangelion3o9@gmail.com>
2018-05-09 10:21:39 -0400

    Initial commit

---
+++ src/main/java/org/aist/aide/generaltypeservice/GeneralTypeServiceApplication.java
@@ -0,0 +1,12 @@
+package org.aist.aide.generaltypeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralTypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralTypeServiceApplication.class, args);
+	}
+}
