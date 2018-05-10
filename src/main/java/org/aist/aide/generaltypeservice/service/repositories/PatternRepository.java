ac67017a0c4dc7fc680de07d0a3d585009475da0
Justin Phillips <euangelion3o9@gmail.com>
2018-05-10 12:00:16 -0400

    Fixed issues wehre Repo wasn't being picked up.

--- src/main/java/org/aist/aide/generaltypeservice/service/repositories/PatternRepository.java
+++ src/main/java/org/aist/aide/generaltypeservice/service/repositories/PatternRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 
 import org.aist.aide.generaltypeservice.domain.models.Pattern;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatternRepository extends MongoRepository<Pattern, UUID> {
     Optional<Pattern> findByType(String type);
 }
