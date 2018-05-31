4a0168fa274c610f1841f588aded259dd1437d1d
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-05-31 14:44:38 -0400

    Added histrix and cleaned build.gradle

--- src/main/java/org/aist/aide/generaltypeservice/GeneralTypeServiceApplication.java
+++ src/main/java/org/aist/aide/generaltypeservice/GeneralTypeServiceApplication.java
@@ -2,9 +2,11 @@ package org.aist.aide.generaltypeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class GeneralTypeServiceApplication {
     public static void main(String[] args) {
